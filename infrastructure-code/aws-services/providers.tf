5f86ae642160131bfad06834494e7538635701f0
Le Hoang Thinh <thinhphuongdong3@gmail.com>
2025-06-27 10:08:38 +0700

    feat(aws): Add the .env for the AWS credential

--- infrastructure-code/aws-services/providers.tf
+++ infrastructure-code/aws-services/providers.tf
@@ -8,7 +8,25 @@ terraform {
 
   required_version = ">= 1.3.0"
 }
+variable "env_file_path" {
+  description = "Path to your custom .env file"
+  type        = string
+  default     = "./.env"
+}
+
+locals {
+  # Properly trim newlines and spaces from file content
+  raw_lines = split("\n", trim(file(var.env_file_path), "\n\r "))
+
+  env_map = {
+    for line in local.raw_lines :
+    trim(split("=", line)[0], "\n\r ") => trim(join("=", slice(split("=", line), 1, length(split("=", line)))), "\n\r ")
+    if length(trim(line, "\n\r ")) > 0 && !startswith(trim(line, "\n\r "), "#")
+  }
+}
 
 provider "aws" {
-  region = "ap-southeast-1"
+  access_key = local.env_map["AWS_ACCESS_KEY_ID"]
+  secret_key = local.env_map["AWS_SECRET_ACCESS_KEY"]
+  region     = local.env_map["AWS_DEFAULT_REGION"]
 }
